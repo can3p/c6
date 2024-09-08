9448644d984f33e7f2a6436f59bd85807e8476f6
Dmitry Petrov <dpetroff@gmail.com>
2024-09-08 10:09:07 +0200

    basic expansion algorithm, need to handle selector permutation

--- runtime/expand.go
+++ runtime/expand.go
@@ -1,18 +1,69 @@
 package runtime
 
-//func ExpandList(scope *Scope, stmts *ast.StmtList) (*ast.StmtList, error) {
-//collector := []ast.Stmt{}
-//out := &ast.StmtList{}
+import (
+	"fmt"
 
-//for _, stmt := range stmts.Stmts {
-//ret, err := ExecuteSingle(scope, stmt)
+	"github.com/c9s/c6/ast"
+)
 
-//if err != nil {
-//return nil, err
-//}
+func ExpandTree(stmts *ast.StmtList) (*ast.StmtList, error) {
+	out := &ast.StmtList{}
 
-//out.AppendList(ret)
-//}
+	for _, stmt := range stmts.Stmts {
+		t, ok := stmt.(*ast.RuleSet)
 
-//return out, nil
-//}
+		if !ok {
+			return nil, fmt.Errorf("Tree can only contain rule sets, but has variable of type %T", stmt)
+		}
+
+		ret, err := expandRuleset(t)
+
+		if err != nil {
+			return nil, err
+		}
+
+		out.AppendList(ret)
+	}
+
+	return out, nil
+}
+
+func expandRuleset(rs *ast.RuleSet) (*ast.StmtList, error) {
+	// empty ruleset does not emit any output
+	if len(rs.Block.Stmts.Stmts) == 0 {
+		return nil, nil
+	}
+
+	out := &ast.StmtList{}
+	collector := []ast.Stmt{}
+
+	for _, stmt := range rs.Block.Stmts.Stmts {
+		switch t := stmt.(type) {
+		case *ast.Property:
+			collector = append(collector, t)
+		case *ast.RuleSet:
+			if len(collector) > 0 {
+				nrs := ast.NewRuleSet()
+				nrs.Selectors = rs.Selectors
+				bl := ast.NewDeclBlock(nrs)
+				bl.AppendList(&ast.StmtList{
+					Stmts: collector,
+				})
+				out.Append(nrs)
+				collector = []ast.Stmt{}
+			}
+
+			expanded, err := expandRuleset(t)
+
+			if err != nil {
+				return nil, err
+			}
+
+			out.AppendList(expanded)
+		default:
+			return nil, fmt.Errorf("Unexpected node type in the expanded tree: %T", t)
+		}
+	}
+
+	return out, nil
+}
