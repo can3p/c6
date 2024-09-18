31e266f12fc989ec1f9549fab943cc426e727d14
Dmitry Petrov <dpetroff@gmail.com>
2024-09-19 01:02:55 +0200

    support for @debug, @warn and @error

--- ast/log_stmt.go
+++ ast/log_stmt.go
@@ -1,9 +1,28 @@
 package ast
 
+type LogLevel int
+
+const (
+	LogLevelDebug LogLevel = iota
+	LogLevelWarn
+	LogLevelError
+)
+
 type LogStmt struct {
 	Directive *Token
+	LogLevel  LogLevel
 	Expr      Expr
 }
 
-func (stm LogStmt) CanBeStmt()     {}
-func (stm LogStmt) String() string { return "LogStmt.String()" }
+func NewLogStmt(e Expr, t *Token, l LogLevel) *LogStmt {
+	return &LogStmt{
+		Directive: t,
+		LogLevel:  l,
+		Expr:      e,
+	}
+}
+
+func (stm LogStmt) CanBeStmt() {}
+func (stm LogStmt) String() string {
+	return "LogStmt.String()"
+}
